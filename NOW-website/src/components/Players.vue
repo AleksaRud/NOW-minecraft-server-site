f13278728baa64a07a55cef476e4c270bf40de7b
AleksaRud <alexandra.rudovich12@gmail.com>
2024-12-13 17:50:15 +0300

    add mobile version of players page

--- NOW-website/src/components/Players.vue
+++ NOW-website/src/components/Players.vue
@@ -4,16 +4,32 @@ import { goToLink, getSource } from './links';
 import { players } from './players';
 
 const activeKey = ref(0);
+const pageWidth = document.documentElement.scrollWidth;
 </script>
 
 <template>
     <div class="tabs">
         <a-tabs v-model:activeKey="activeKey" centered>
-            <a-tab-pane v-for="(season_group, id) in players" :key="id" :tab="(players.length >= 2) ? season_group.season : ''" class="tab">
+            <a-tab-pane v-for="(season_group, id) in players" :key="id" :tab="(players.length >= 2) ? season_group.season : ''" class="tab"  v-if="pageWidth > 425">
                 <div v-for="(player, ind) in season_group.players_list" class="player" v-bind:style="(ind % 2) ? {flexDirection:'row-reverse'} : {flexDirection:'row'}">
                     <div class="card" v-bind:style="{ background: 'url(' + player.card + '), #5a8fba', backgroundPosition: 'center', backgroundSize: 'cover' }"></div>
                     <div class="about">
                         <div class="nickname">{{ player.nickname }}</div>
+                        <div class="info" v-html="player.info"></div>
+                        <div v-if="player.links" class="links">
+                            <a-button v-for="link in player.links" class="btn" @click = goToLink(link)>{{ getSource(link) }}</a-button>
+                        </div>
+                    </div>
+                </div>
+            </a-tab-pane>
+            <a-tab-pane v-for="(season_group, id) in players" :key="id" :tab="(players.length >= 2) ? season_group.season : ''" class="tab"  v-if="pageWidth <= 425">
+                <div v-for="(player, ind) in season_group.players_list" class="player">
+                    <div class="card-and-name" v-bind:style="(ind % 2) ? {flexDirection:'row-reverse'} : {flexDirection:'row'}">
+                        <div class="card" v-bind:style="{ background: 'url(' + player.card + '), #5a8fba', backgroundPosition: 'top', backgroundSize: 'cover' }"></div>
+                        <div class="nickname">{{ player.nickname }}</div>
+                    </div>
+                    
+                    <div class="about">
                         <div class="info" v-html="player.info"></div>
                         <div v-if="player.links" class="links">
                             <a-button v-for="link in player.links" class="btn" @click = goToLink(link)>{{ getSource(link) }}</a-button>
@@ -70,5 +86,43 @@ const activeKey = ref(0);
         box-sizing: border-box;
         height: 48px;
         width: 144px;
+    }
+    @media(max-width: 425px) {
+        .tabs{
+            padding: 20px 0 40px 0;
+            width: 80%;
+        }
+        .tab{
+            font-size: 16px;
+        }
+        .player{
+            flex-direction: column;
+            justify-content: flex-start;
+            gap: 20px;
+        }
+        .card-and-name{
+            display: flex;
+            flex-direction: row;
+            gap: 48px;
+        }
+        .card{
+            min-width: 120px;
+            min-height: 120px;
+            height: 120px;
+            border-radius: 20px;
+        }
+        .about{
+            width: 100%;
+            gap: 16px;
+        }
+        .nickname{
+            font-size: 20px;
+            padding-top: 20px;
+        }
+        .btn{
+            box-sizing: border-box;
+            height: 36px;
+            width: 100%;
+        }
     }
 </style>
